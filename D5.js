c04f973328f4dd98c124c868c9b6089bcd47187d
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-06-25 14:24:21 +0100

    Fixed exercise 1, completed 2 and 3

--- D5.js
+++ D5.js
@@ -10,21 +10,47 @@ ASSIGNMENT RULES
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
 
-var l1 = ()
-var l2 = ()
 
-let area = function(l1, l2) {
+
+const area = function (l1, l2) {
     return l1 * l2;
 }
+ 
+let result = area(2, 4)
+
+console.log(result)
+
 
 /* EXERCISE 2
 Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
+
+
+const crazySum = function(a, b) {
+  
+    
+    if (a === b) {
+        return a + b * 3;
+    } 
+     return a + b;
+}
+ let crazySumResult =crazySum(1, 2)
+ console.log(crazySumResult)
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
 It should return triple their absolute difference if the given number is greater than 19.
 */
+
+
+
+const crazyDiff = function(input) {
+    return input - 19;
+}
+
+crazySum(20)
+
+crazySum(2)
 
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
